0cad2da9e563974feccca1c8cfc3ef3be31c3c80
networkSorcerer <gigattfyhig@gmail.com>
2024-12-09 12:19:19 +0900

    admin

--- src/main/java/com/kh/MINI/admin3/controller3/OrderController3.java
+++ src/main/java/com/kh/MINI/admin3/controller3/OrderController3.java
@@ -1,6 +1,7 @@
 package com.kh.MINI.admin3.controller3;
 
 import com.kh.MINI.admin3.dao3.OrderDAO3;
+import com.kh.MINI.admin3.vo3.CustomPCVO3;
 import com.kh.MINI.admin3.vo3.OrdersVO3;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +20,20 @@ import java.util.Map;
 public class OrderController3 {
     private final OrderDAO3 orderDAO3;
 
-    @GetMapping("orderList/{user_id}")
+    // 개별 상품 주문 목록 조회
+    @GetMapping("/orderList/{user_id}")
     public Map<String, Object> orderList (@PathVariable int user_id) {
         Map<String, Object> resultMap = new HashMap<>();
         List<OrdersVO3> orderList = orderDAO3.orderList(user_id);
         resultMap.put("orderList", orderList);
         return resultMap;
+    }
+
+    // 커스텀 상품 주문 목록 조회
+    @GetMapping("/customOrderList/{user_id")
+    public Map<String, Object> customOrderList(@PathVariable int user_id) {
+        Map<String, Object> resultMap = new HashMap<>();
+        List<CustomPCVO3>customOrderList = orderDAO3.
     }
 
 }
