da29f97db686af8281a5f5d6993ec8bc74d58ae3
networkSorcerer <gigattfyhig@gmail.com>
2024-12-10 10:35:01 +0900

    admin3

--- React_FRONT/src/App.js
+++ React_FRONT/src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil"; // RecoilRoot 임포트
 import Home from "./pages/Home.js";
 import Login from "./pages/signup/Login.js";
 import Signup from "./pages/signup/Signup";
-import AdminHome from "./pages/admin/AdminHome";
-import { UserContextProvider } from "./api/provider/UserContextProvider.js";
-import { AdminUsersMap } from "./api/provider/UserSearchContextProvider.js";
-import AdminUserOrder from "./pages/admin/AdminOrders/AdminUserOrder.js";
 import Header4 from "./components/Header4";
 import Body4_1 from "./components/Body4_1";
 import Body4_2 from "./components/Body4_2";
 import Footer4 from "./components/Footer4";
 import ProductDetail4 from "./product/ProductDetail4"; // 상세 페이지 컴포넌트 추가
+import Shopping01 from "./pages/Shopping01";
+import UserPage01 from "./pages/UserPage01";
+import ProductSortComponent from "./components/ProductSortComponent01";
+import OwnPC from "./components/OwnPC";
+import SuggestedPC from "./components/SuggestedPC";
+import Order from "./components/Order";
+import OrderSuccess from "./components/OrderSuccess";
+import AdminHome from "./pages/admin/AdminHome";
+import Address from "./pages/signup/address.js";
+import EmailVerification from "./pages/admin/EmailVerification.js";
+import DragAndDropThreeLists from "./pages/admin/TableDrad.js";
+import SwiperExample from "./pages/admin/Swiper.js";
+import Swiper2 from "./pages/admin/Swiper2.js";
+import ProductSwiper from "./pages/admin/ImageArray.js";
+import { UserContextProvider } from "./api/provider/UserContextProvider.js";
+import { AdminUsersMap } from "./api/provider/UserSearchContextProvider.js";
+import AdminUserOrder from "./pages/admin/AdminOrders/AdminUserOrder.js";
 
 function App() {
   return (
