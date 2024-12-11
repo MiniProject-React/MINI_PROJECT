6d417bfeba938813c6b1a46128734880fe8241cf
networkSorcerer <gigattfyhig@gmail.com>
2024-12-11 17:47:24 +0900

    admin3

--- React_FRONT/src/pages/admin/AdminOrders/AdminOrderProducts.js
+++ React_FRONT/src/pages/admin/AdminOrders/AdminOrderProducts.js
@@ -2,13 +2,13 @@ import { useContext, useEffect, useState } from "react";
 import AxiosApi from "../../../api/AxiosApi3";
 import { PageNavigate } from "../../../api/Pagination/PageNavigate";
 import { ProductsSearchContext } from "../../../api/provider/ProductsSearchContextProvider";
-import { CheckBox } from "@material-ui/icons";
 
 const AdminOrderProducts = () => {
   const { searchKeyword } = useContext(ProductsSearchContext);
   const [totalCnt, setTotalCnt] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [productList, setProductList] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
   useEffect(() => {
     ProductList();
@@ -26,6 +26,24 @@ const AdminOrderProducts = () => {
     setProductList(rsp.data.order_products);
     setCurrentPage(cpage);
     setTotalCnt(rsp.data.totalCount);
+  };
+
+  // 체크박스 값 변경 처리
+  const handleCheckboxChange = (product) => {
+    setSelectedProducts((prevSelected) => {
+      const newSelected = { ...prevSelected };
+      const productId = product.product_id;
+
+      if (newSelected[productId]) {
+        // 이미 선택된 경우, 체크 해제
+        delete newSelected[productId];
+      } else {
+        // 선택되지 않은 경우, 체크
+        newSelected[productId] = true;
+      }
+
+      return newSelected;
+    });
   };
 
   return (
@@ -52,6 +70,8 @@ const AdminOrderProducts = () => {
                       type="checkbox"
                       value={product.product_id}
                       className="form-check-input"
+                      checked={selectedProducts[product] || false}
+                      onChange={() => handleCheckboxChange(product)}
                     />
                   </td>
                   <td>{product.category}</td>
@@ -69,6 +89,26 @@ const AdminOrderProducts = () => {
             )}
           </tbody>
         </table>
+      </div>
+      <div>
+        <h4>선택된 상품</h4>
+        <div>
+          {selectedProducts && selectedProducts.length > 0 ? (
+            selectedProducts.map((select) => (
+              <div key={select.product_id}>
+                <strong>{select.category}</strong>
+                {select.product}
+              </div>
+            ))
+          ) : (
+            <div>
+              <strong>상품을 선택해주세요</strong>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <button>주문 하기</button>
       </div>
       <div className="d-flex justify-content-center">
         <PageNavigate
